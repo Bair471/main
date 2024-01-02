b6b4c35ef1d2ebba8416d2f07114562d123ced06
Bair Shirnengiyn <39797457+Bair471@users.noreply.github.com>
2024-01-02 23:20:57 +0800

    Commite 57

--- frontend/index.js
+++ frontend/index.js
@@ -21,6 +21,14 @@ fetch(`http://localhost:8000/api/bikes`)
         model.innerText = bike.model;
         year.innerText = bike.year;
         price.innerText = bike.price;
+
+        
+        const editButton = document.createElement('button');
+        editButton.className = 'btn btn-primary';
+        editButton.innerText = 'Edit';
+        editButton.addEventListener('click', () => {
+            window.location.href = `../edit-bike/edit-bike.html?id=${bike._id}`;
+        });
     })
 })
 
