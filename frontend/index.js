a0ce3d0425c2eb4d63e8c127f52a085b1717c2b1
Bair Shirnengiyn <39797457+Bair471@users.noreply.github.com>
2024-01-02 23:15:13 +0800

    Commite 52

--- frontend/index.js
+++ frontend/index.js
@@ -1,22 +1,12 @@
-function handleSave() {
-   const info = document.getElementById(`info`)
-   alert(info.value)
-   const brand = document.getElementById(`brand`)
-   alert(brand.value)
-fetch(`http://localhost:8080/info`,{method:'POST',
-headers: {
-    'Content-Type': 'application/json'
-},
-body: JSON.stringify({
-    information: info.value, 
-    brand: brand.value,
-    
-})
-})
-.then(response => response.text())
-.then(result => console.log(`Tochno? ${result}`))
-.catch(error => console.log('error', error));
-}
 
+const bikesbody = document.getElementById('bikes-body');
+
+fetch(`http://localhost:8000/api/bikes`)
+.then(response => response.json())
+.then(bikes => {(b)
+    bikes.forEach(bike => {
+        
+    })
+})
 
 
