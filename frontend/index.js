0d1d27bd8905a4a1bbbad0980148425c3127a55f
Bair Shirnengiyn <39797457+Bair471@users.noreply.github.com>
2024-01-02 23:16:41 +0800

    Commite 52

--- frontend/index.js
+++ frontend/index.js
@@ -5,7 +5,22 @@ fetch(`http://localhost:8000/api/bikes`)
 .then(response => response.json())
 .then(bikes => {(b)
     bikes.forEach(bike => {
+        const row = document.createElement('tr');
+
+    
+        const id = document.createElement('td');
+        const brand = document.createElement('td');
+        const model = document.createElement('td');
+        const year = document.createElement('td');
+        const price = document.createElement('td');
+        const actions = document.createElement('td');
+
         
+        id.innerText = bike._id;
+        brand.innerText = bike.brand;
+        model.innerText = bike.model;
+        year.innerText = bike.year;
+        price.innerText = bike.price;
     })
 })
 
