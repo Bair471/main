20a6fbc7af84000dbeedac07f00e824a175edcc2
Bair Shirnengiyn <39797457+Bair471@users.noreply.github.com>
2024-01-06 21:10:23 +0800

    Commit60

--- backend/index.js
+++ backend/index.js
@@ -97,86 +97,104 @@ app.listen(port, () => console.log(`Server listening on port ${port}!`));
 
 
 
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const {MongoClient, ObjectId} = require('Mongodb');
 
-// const app = express();
-// app.use(cors());
-// app.use(bodyParser.json())
+app.put('/api/bikes2/:id', async (req, res) => {
+    const id = req.params.id;
+    const bike = req.body;
 
-// const mongoConnectionString = process.env.MONGO_CONNECTION_STRING;
+    // Validation
+    if (!bike.brand || !bike.model || isNaN(bike.year) || isNaN(bike.price)) {
+        res.status(400).send('Missing required fields: brand, model, year (number), price (decimal)');
+        return;
+    }
 
-// app.get('/app/bikes', (req, res) => {
-//     const client = await MongoClient.connect(mongoConnectionString);
-//     const db = client.db('bikes');
-//     const result = await db.collection('data').find().toArray();
+    const client = await MongoClient.connect(mongoConnectionString);
+    const db = client.db('bikes');
+    const result = await db.collection('data').updateOne({ _id: new ObjectId(id) }, {
+        $set: {
+            brand: bike.brand,
+            model: bike.model,
+            year: parseInt(bike.year),
+            price: parseFloat(bike.price)
+        }
+    });
 
-//     res.send('result');
-// });
+    res.send(result);
+});
 
 
+app.get('/app/bikes/:id', async (req, res) => {
+    const id = req.params.id;
+    const bike = req.body;
+
+    const client = await MongoClient.connect(mongoConnectionString);
+    const db = client.db('bikes');
+    const result = await db.collection('data').updateOne({ _id: new ObjectId(id) }, {
+        $set: {
+            brand: bike.brand,
+            model: bike.model,
+            year: parseInt(bike.year),
+            price: parseFloat(bike.price)
+        }
+    });
+
+    res.send(result);
+})
 
 app.get('/api/bikes/:id', async (req, res) => {
     const id = req.params.id;
     const client = await MongoClient.connect(mongoConnectionString);
     const db = client.db('bikes');
-
-    const result = await db.collection('data').findOne({ _id: new ObjectId(id) });
-
+    const result = await db.collection('data').updateOne({ _id: new ObjectId(id) }, {
+        $set: {
+            brand: bike.brand,
+            model: bike.model,
+            year: parseInt(bike.year),
+            price: parseFloat(bike.price)
+        }
+    });
     res.send(result);
 });
 
-app.get('/app/bikes/:id', async (req, res) => {
+app.get('/api/bikes/:id', async (res, req) =>{
     const id = req.params.id;
+    const bike = req.body;
     const client = await MongoClient.connect(mongoConnectionString);
     const db = client.db('bikes');
+    const result = await db.collection('data').updateOne({ _id: new ObjectId(id)}, {
+        $set: {
+            brand: bike.brand,
+            model: bike.model,
+            year: parseInt(bike.year),
+            price: parseFloat(bike.price)
+        }
+    })
+}) 
 
-    const result = await db.collection('data').findOne({ _id: new ObjectId(id) });
-
-    res.send(result);
-})
 
 
-app.get('/app/bikes/:id', async (req, res) =>{
+
+
+
+
+
+
+app.put('/api/bikes3/:id', async (req, res) => {
     const id = req.params.id;
+    const bike = req.body;
     const client = await MongoClient.connect(mongoConnectionString);
     const db = client.db('bikes');
-
-    const result = await db.collection('data').findOne({ _id: new ObjectId(id) });
-
-    res.send(result);
-})
-
-
-app.get('/app/bikes/:id', async (req, res) => {
-    const id = req.params.id;
-    const client = await MongoClient.connect(mongoConnectionString);
-    const db = client.db('biles');
-
-    const result = await db.collection('data').findOne({ _id: new ObjectId(id) });
-
-    res.send(result);
-})
-
-
-app.delete('/api/bikes/:id', async (req, res) => {
-    const id = req.params.id;
-
-    const client = await MongoClient.connect(mongoConnectionString);
-    const db = client.db('bikes');
-    const result = await db.collection('data').deleteOne({ _id: new ObjectId(id) });
-
-    res.send(result);
-});
-
-
-app.delete('/app/bikes/:id', async (req, res) => {
-    const id = req.params.id;
-    const client = await MongoClient.connect(mongoConnectionString);
-    const db = client.db('bikes');
-    const result = await db.collection('data').deleteOne({ _id: ObjectId(id) });
-
+    const collection = db.collection('data');
+    const result = await collection.updateOne({_id: new ObjectId(id)},
+        {
+          $set: {
+            brand: bike.brand,
+            model: bike.model,
+            type: bike.type,
+            year: parseInt(bike.year),
+            price: parseFloat(bike.price)
+        },          
+        }
+      );
     res.send(result);
 })
