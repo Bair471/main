38becafbb0ffd0e92821b009c7bab81ec9d288bb
Bair Shirnengiyn <39797457+Bair471@users.noreply.github.com>
2024-01-12 21:38:10 +0800

    Commite 79

--- frontend/pages/add-bike/add-bike.js
+++ frontend/pages/add-bike/add-bike.js
@@ -33,15 +33,29 @@ function addBike(event) {
     event.preventDefault();
 
     const form = event.target;
-    
-    const bike ={
+
+    const bike = {
         brand: form.brand.value,
         model: form.model.value,
         year: form.year.value,
         price: form.price.value
     };
+}
 
-    fetch('http://localhost:8000/api/bikes', {
+function addBike(event) {
+    event.preventDefault();
+
+    const form =event.target;
+
+    const bike = {
+        brand: form.brand.value,
+        model: form.model.value,
+        year: form.year.value,
+        price: form.price.value
+    }
+}
+
+fetch('http://localhost:8000/api/bikes', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -57,18 +71,4 @@ function addBike(event) {
             console.error('Failed to add a bike:', error);
         });
 }
-    fetch('http://localhost:8000/api/bikes', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(bike)
-    })  
-        .then(response => response.json())
-        .then(result => {
-            console.log(result);
-            wirndow.location.href = '../bike-list/bike-list.html';
-        })
-        .catch(error => {
-            console.error('Failed to add a bike:', error);
-        });
+
