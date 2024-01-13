28bc4fac6426fc71c0b59163e7f65b28353adb0a
Bair Shirnengiyn <39797457+Bair471@users.noreply.github.com>
2024-01-13 19:12:41 +0800

    commit80

--- frontend/pages/add-bike/add-bike.js
+++ frontend/pages/add-bike/add-bike.js
@@ -29,25 +29,32 @@ function addBike(event) {
 
 
 
-function addbike(event) {
+function addBike(event)  {
     event.preventDefault();
 
-    const form = event.target;
+    const form = evenet.target;
 
     const bike = {
-        brand: form.brand.value,
-        model: form.brand.value,
-        year: form.brand.value,
-        price: form.price.value
+        brand:form.brand.value,
+        model:form.model.value,
+        year:form.year.value,
+        price:form.year.value
+};
 
-    
-    };
-    fetch ('http://localhost:8000/api/bikes'), {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(bike)
-    }
+fetch('http://localhost:8000/api/bikes', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'aplication/json'
+    },
+    body: JSON.stringify(bike)
+
+})  
+    .then(resposnes => respones.json())
+    .then(result => {
+        console.log(result);
+        window.location.href = '../bike-list/bike-list.html';
+    })
+    .catch(error => {
+        console.error('Failed to add a bike:', error);
+    });
 }
-
